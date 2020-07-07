0cfac29f8cc213965bc28b453e176429ba96b43c
eazytrainingfr <66882176+eazytrainingfr@users.noreply.github.com>
2020-07-07 18:10:33 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0083662ba17882949"
+  instance_type = "t2.micro"
+  key_name      = "devops-dirane"
+  tags = {
+    Name = "ec2-dirane"
+  }
+}
