13305adb2e7290f98570ac3fb08b5f12da1fae27
safamarzouki-code <126520713+safamarzouki-code@users.noreply.github.com>
2023-03-18 12:42:39 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-0083662ba17882949"
+  ami           = "ami-012cc038cc685a0d7"
   instance_type = "t2.micro"
   key_name      = "devops-dirane"
   tags = {
